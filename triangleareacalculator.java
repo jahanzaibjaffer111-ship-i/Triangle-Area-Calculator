7b2a8c634641fce6dbe0e776542c759b553eab02
Jahanzaib Jaffer Ali <jahanzaib.jaffer111@gmail.com>
2026-01-12 12:28:32 +0500

    Add files via upload

---
+++ triangleareacalculator.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+public class triangleareacalculator
+{
+    public static void main(String[] args)
+    {
+        /*a=area
+        s=semi perimeter
+        b=first length
+        c=second length
+        d=thirth length */
+        double a,s,b,c,d;
+        try (Scanner read = new Scanner(System.in)) {
+            while(1>0)
+            {
+                System.out.println("HELLO");
+                System.out.println("WELCOME TO TRIANGLE AREA CALCULATOR");
+                System.out.println("ENTRE FIRST LENGTH OF TRIANGLE");
+                b=read.nextDouble();
+                if(b>0)
+                {
+                    System.out.println("ENTRE SECOND LENGTH OF TRIANGLE");
+                    c=read.nextDouble();
+                    if(c>0)
+                    {
+                        System.out.println("ENTRE THIRTH LENGTH OF TRIANGLE");
+                        d=read.nextDouble();
+                        if(d>0)
+                        {
+                            s=(b+c+d)/2;
+                            a =Math.sqrt(s*(s-b)*(s-c)*(s-d));
+                            System.out.println("SEMI PERIMETER OF TRIANGLE IS");
+                            System.out.println(s);
+                            System.out.println("AREA OF TRINAGLE IS");
+                            System.out.println(a);
+                            System.out.println("THANKS");
+                            System.out.println("BYE");
+                            System.out.println(".");
+                        }
+                        if(d<0)
+                        {
+                            System.out.println("LENGHT OF TRIANGLE CANT BE NEGATIVE");
+                            System.out.println("TRY AGAIN");
+                            System.out.println(".");
+                        }
+                    }
+                    if(c<0)
+                    {
+                        System.out.println("LENGHT OF TRIANGLE CANT BE NEGATIVE");
+                        System.out.println("TRY AGAIN");
+                        System.out.println(".");
+                    }
+                }
+                if(b<0)
+                {
+                    System.out.println("LENGHT OF TRIANGLE CANT BE NEGATIVE");
+                    System.out.println("TRY AGAIN");
+                    System.out.println(".");
+                }
+            }
+        }
+    }
+}
